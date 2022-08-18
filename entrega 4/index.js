26293444130dbe568756e03dc791247588e22a60
SuciaRata <gsebastianezequiel@gmail.com>
2022-08-18 10:28:10 -0300

    Se crea el github

---
+++ entrega 4/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { Router } = express
+const ProductosApi = require('./api/productos.js')
+
+// router de productos
+
+const productosApi = new ProductosApi()
+
+const productosRouter = new Router()
+
+productosRouter.use(express.json())
+productosRouter.use(express.urlencoded({ extended: true }))
+
+productosRouter.use(express.static( __dirname + '/public'));
+
+//rutas usando productosRouter
+
+
+
+// servidor
+
+const app = express()
+app.use(express.static('public'))
+app.use('/api/productos', productosRouter)
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+server.on("error", error => console.log(`Error en servidor ${error}`))
